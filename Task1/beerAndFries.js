eb6daec54c04960311606ec0550b7f008a9bdd78
antony.dikov <antony.dikov@imperiaonline.org>
2014-09-19 09:08:54 +0300

    Added unit tests and unpolluted scope.

--- Task1/beerAndFries.js
+++ Task1/beerAndFries.js
@@ -1,31 +1,46 @@
-function calcScore(sortedBeers, sortedFries) {
+(function () {
+    "use strict";
+    function calcScore(sortedBeers, sortedFries) {
         var i,
                 beersAmount = sortedBeers.length,
                 friesAmount = sortedFries.length,
                 length = beersAmount < friesAmount ? beersAmount : friesAmount,
                 result = 0;
-        for(i = 0; i < length; i++) {
-                result += sortedBeers[i].score * sortedFries[i].score;
+        for (i = 0; i < length; i++) {
+            result += sortedBeers[i].score * sortedFries[i].score;
         }
-        if(length < beersAmount) {
-                for(i; i < beersAmount; i++) {
-                        result += sortedBeers[i].score;
-                }
+        if (length < beersAmount) {
+            for (i; i < beersAmount; i++) {
+                result += sortedBeers[i].score;
+            }
         } else {
-                for(i; i < friesAmount; i++) {
-                        result += sortedFries[i].score;
-                }
+            for (i; i < friesAmount; i++) {
+                result += sortedFries[i].score;
+            }
         }
         return result;
-}
- 
-exports.beerAndFries = function beerAndFries(items) {
-        var beerContainer = items.filter(function(item){if(item){return item.type === 'beer';}}),
-                friesContainer = items.filter(function(item){if(item){return item.type === 'fries';}});
-       
-        beerContainer.sort(function(firstItem, secondItem){ return secondItem.score - firstItem.score;});
-       
-        friesContainer.sort(function(firstItem, secondItem){ return secondItem.score - firstItem.score;});
-       
+    }
+
+    exports.beerAndFries = function beerAndFries(items) {
+        var beerContainer = items.filter(function (item) {
+            if (item) {
+                return item.type === 'beer';
+            }
+        }),
+            friesContainer = items.filter(function (item) {
+                if (item) {
+                    return item.type === 'fries';
+                }
+        });
+
+        beerContainer.sort(function (firstItem, secondItem) {
+            return secondItem.score - firstItem.score;
+        });
+
+        friesContainer.sort(function (firstItem, secondItem) {
+            return secondItem.score - firstItem.score;
+        });
+
         return calcScore(beerContainer, friesContainer);
-}
+    }
+})();
